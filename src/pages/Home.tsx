f00596187e74fa1cf9b91382806ea25510cf0e56
Alik Rodrigues <alik.rodrigues@pluks.com.br>
2021-09-13 08:17:50 -0300

    Finalizando desafio 01

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,27 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    const task = { id: new Date().getTime(), title: newTaskTitle, done: false };
+    setTasks([...tasks, task]);
     //TODO - add new task
   }
 
   function handleToggleTaskDone(id: number) {
+    const updatedTask = tasks.find((item) => item.id === id);
+    if (updatedTask) {
+      updatedTask.done = !updatedTask.done;
+      const newTasks = tasks.filter((item) => item.id !== id);
+      newTasks.push(updatedTask);
+      setTasks(newTasks);
+    }
+
+
     //TODO - toggle task done if exists
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const newTasks = tasks.filter((item) => item.id !== id);
+    setTasks(newTasks);
   }
 
   return (
@@ -26,10 +38,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
